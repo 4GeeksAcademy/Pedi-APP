699530330d1c51cbadd81897fd6d379d56a3b8bf
mariaperez15 <123765659+mariaperez15@users.noreply.github.com>
2023-06-21 18:44:25 +0000

    empiece

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { SingupCliente } from "./store/signupCliente";
 
 //create your first component
 const Layout = () => {
@@ -26,6 +27,7 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
+                        <Route element={<SingupCliente/>} path="/signupCliente" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
