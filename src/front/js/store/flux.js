035f8b07ace81db80817a88acf66fa6d25b8841c
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-06-23 12:10:39 +0000

    finished login function, just need to add userdata to the store

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,24 +1,43 @@
+import Swal from "sweetalert2";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			
+			isloged: false,
+			current_user_data: {
+
+			}
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			
+			login_handlinator: async (user) => {
+				try {
+					const response = await fetch(process.env.BACKEND_URL + "/api/login", {
+						method : "POST",
+						body: JSON.stringify(user),
+						headers: {
+							'Content-Type': 'application/json'
+						}
+					})
+					const result = await response.json()
+					Swal.fire(result.message)
+					if (response.status == 200){
+						localStorage.setItem("jwt-token", result.token);
+						setStore({isloged:true})
+						console.log(result)
+						/*  EDITAR ESTO CON LA INFO DEL USUARIO QUE HAGA FALTA*/
+						setStore({current_user_data:result.userdata})
+						console.log(getStore())
+						return true
+					} else {
+						return false
+					}
+
+				}catch(error){
+					console.log("Error loading message from backend")
+				}		
 			}
 			
 
