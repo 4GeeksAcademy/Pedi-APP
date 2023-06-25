13d4b121c01cd02bd78ca72690f5b6087ff2dde3
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-06-25 14:17:28 +0000

    started homepage

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,8 +7,20 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			
+		<div className="text-center container-fluid ">
+			<div className="row home_first_row">
+				<h1 className="home_title"> DishDash</h1>
+				<p className="home_subtitle"> Dashing to your door</p>
+
+				<form>
+					<div className="mb-3 ">
+						<input type="email" className="form-control" id="search" aria-describedby="emailHelp"/>
+					</div>
+				</form>
+			</div>
+			<div className="row">
+
+			</div>
 		</div>
 	);
 };
