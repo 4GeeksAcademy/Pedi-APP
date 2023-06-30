d119bb991139db6c6f28d10592482f0fc095a1a3
Diego Casermeiro Gálvez <122561493+diegokser@users.noreply.github.com>
2023-06-30 21:31:40 +0000

    cambios en footer y navbar

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,9 +3,9 @@ import "../../styles/footer.css";
 import Swal from "sweetalert2";
 
 export const Footer = () => (
-  <footer className="py-3  border-top  footer_container">
+  <footer className="py-3 border-top  footer_container">
     <div className="row footer_row ">
-		<div className="col-2 footer_col ">
+		<div className="col-6 col-lg-2 footer_col mt-3">
 			<div className="footer_logo mb-2">
 				<button type="button" className="btn btn-danger footer_logo_btn">DishDash</button>
 			</div>
@@ -17,18 +17,18 @@ export const Footer = () => (
 			</div>
 
 		</div>
-		<div className="col-2 footer_col mt-3 ">
+		<div className="col-6 col-lg-2 footer_col mt-3">
 			<div className="footer_store mb-2 ">
 				<button type="button" className="btn btn-danger footer_store_btn mb-4 ">
 					<div className="footer_appstore ">
-						<i className="fab fa-apple fa-3x me-2"></i>
+						<i className="fab fa-apple me-2"></i>
 						<p className="footer_appstore_text mt-3">Download on the<br/><strong className="footer_appstore_text1 mt-3">App Store</strong></p>
 					</div>
 					
 				</button>
 				<button type="button" className="btn btn-danger footer_store_btn mb-4">
-					<div className="footer_appstore ">
-						<i className="fab fa-google-play fa-2x me-2"></i>
+					<div className="footer_appstore">
+						<i className="fab fa-google-play me-2"></i>
 						<p className="footer_google-play_text mt-3">Get it on <br/><strong className="footer_google-play_text1 mt-3">Google Play</strong></p>
 					</div>
 					
@@ -36,14 +36,14 @@ export const Footer = () => (
 			</div>
 
 		</div>
-		<div className="col-2 footer_col  ">
+		<div className="col-4 col-lg-2 footer_col  ">
 			<div className="footer_infotext_container ">
 				<p className="footer_infotext">Information</p>
 				<p className="footer_infosubtext"> About us <br/> Contact <br/>Promotions<br/> DishDash Prime</p>
 			</div>
 
 		</div>
-		<div className="col-2 footer_col  ">
+		<div className="col-4 col-lg-2 footer_col  ">
 			<div className="footer_infotext_container ">
 				<p className="footer_infotext ">Lets do it together</p>
 				<p className="footer_infosubtext"> Become a rider <br/>Add your business</p>
@@ -56,7 +56,7 @@ export const Footer = () => (
 			</div>
 
 		</div>
-		<div className="col-2 footer_col  ">
+		<div className="col-4 col-lg-2 footer_col  ">
 			<div className="footer_infotext_container ">
 				<p className="footer_infotext">Helpfull links</p>
 				<p className="footer_infosubtext"> Terms and conditions <br/> Privacy policy <br/>Cookies policy<br/> Compliance</p>
