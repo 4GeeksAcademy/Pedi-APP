b72b463521553ff28dd46b35221c04ecc9435357
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-07-04 17:10:59 +0200

    Update signupEmpresa.js

--- src/front/js/pages/signupEmpresa.js
+++ src/front/js/pages/signupEmpresa.js
@@ -81,7 +81,7 @@ export const SingupEmpresa = () => {
             return Swal.fire("You have to agree to Terms and Conditions to be able to signup")
           }
         else{
-            const register = actions.signupEmpresa(formData.nombre, formData.cif, formData.calleNumero, formData.pisoPuerta, formData.codigoPostal, formData.estado, formData.ciudad, formData.delivery, formData.reserva, formData.mañana, formData.tarde, formData.terminosCondiciones);
+            const register = actions.signupEmpresa(formData.nombre, formData.cif, formData.calleNumero, formData.pisoPuerta, formData.codigoPostal, formData.estado, formData.ciudad, formData.delivery, formData.reserva, formData.mañana, formData.tarde, formData.img);
             if (register) {navigate('/', { replace: true });}
             else {
                  return Swal.fire ("Address not found try again")
