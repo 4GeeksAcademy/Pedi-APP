5328cf2a5db54ad9307ac1021822dbb4c6c1e163
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-07-07 15:24:01 +0000

    started subtotal modal

--- src/front/js/component/user_order.js
+++ src/front/js/component/user_order.js
@@ -27,7 +27,43 @@ const User_order = () =>{
                     <div className="row order_secondrow">
                         <div className="col-5 order_amount_box ">
                             <p className="order_amount ">2 items for 30$</p>
-                            <p className="order_recipt ">- View recipt</p>
+                            <a className=" ms-3 order_recipt" data-bs-toggle="modal" data-bs-target="#reciptmodal">View recipt</a>
+                                    <div className="modal fade" id="reciptmodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div className="modal-dialog">
+                                            <div className="modal-content">
+                                            <div className="modal-header">        
+                                            <button type="button" class="btn-close " data-bs-dismiss="modal" aria-label="Close"></button>            
+                                                <div className="row align-self-start"> 
+                                                    <div className="col"> 
+                                                        <p className="text-light ms-2 mt-2">Thanks for ordering with</p> 
+                                                    </div>
+                                                </div>
+                                                <div className="row  w-100 modal_row_header"> 
+                                                   <div className="col-8  p-0 "> 
+                                                        <h1 className="text-light text-center  "> DishDash</h1>
+                                                   </div>
+                                                   <div className="col-4 modal_price_col p-0 h-100">
+                                                        <p className=" text-light  text-end"> 30$</p>
+                                                        <p className=" text-light  text-end my-0"> 05/05/1999</p>
+                                                   </div>
+                                                </div>
+                                                
+                                            </div>
+                                            <div className="modal-body">
+                                            <ul className="ps-5 border">
+                                                <li> <h5 className="mt-3">Big mac</h5> </li>
+                                                <li> <h5 className="mt-3">whooper</h5> </li>
+                                                
+                                            </ul>
+                                            </div>
+                                            <div className="modal-footer border">
+                                                <div className="row border"> 
+                                                    <p className="modal_price_footer"> Subtotal </p> 
+                                                </div>
+                                            </div>
+                                            </div>
+                                        </div>
+                                    </div>
                         </div>
                         
                     </div>
