78091081e73568b8fc4daa7e228d5cc8c104a8af
diuca puig <diucapuig@gmail.com>
2023-07-19 17:16:16 +0000

    fixed companypage menu

--- src/front/js/component/companyPageMenu.js
+++ src/front/js/component/companyPageMenu.js
@@ -23,7 +23,7 @@ export const CompanyPageMenu = ({ idEmpresa }) => {
     if (store.current_user_data.role == "Empresa") {
       Swal.fire("Must be a client to buy!");
     } else if (!store.current_user_data.role) {
-      Swal.fire("Must be loged to buy!");
+      Swal.fire("Must be logged to buy!");
     } else {
       actions.buyProduct(nombre, precio, descripcion, img, cantidad, id);
       navigate("/orderDetail", { replace: true });
@@ -51,43 +51,41 @@ export const CompanyPageMenu = ({ idEmpresa }) => {
     })();
   }, []);
 
-
-    return(
-        <div className="container-fluid">
-            <div className="tab-pane fade show active" id="nav-menu" role="tabpanel" aria-labelledby="nav-home-tab" tabIndex="0">
-                    <div className="row row-cols-2 d-flex justify-content-evenly">
-                    {products.map((product,index)=>{
-                            return (
-                        <div className="col-11 col-md-5 d-flex border menu_container_page" key={index}>
-                                <div className="col-3 menu_imgbox">
-                                    <img src={product && product.img} alt="foto producto" className="page_company_img" />
+  return(
+    <div className="container-fluid">
+        <div className="tab-pane fade show active" id="nav-menu" role="tabpanel" aria-labelledby="nav-home-tab" tabIndex="0">
+                <div className="row row-cols-2 d-flex justify-content-evenly">
+                {products.map((product,index)=>{
+                        return (
+                    <div className="col-11 col-md-5 d-flex border menu_container_page" key={index}>
+                            <div className="col-3 menu_imgbox">
+                                <img src={product && product.img} alt="foto producto" className="page_company_img" />
+                            </div>
+                            <div className="menu_text_page">
+                                <div className="d-flex container-tittle-icon">
+                                    <div className="menu_title_page">
+                                        <h4 className="menu_title">{product && product.nombre}</h4>
+                                    </div>
+                                    <button className="btn menu_icono"  onClick={(e) => handleBuyProduct(e, product.nombre, product.precio, product.descripcion, product.img, 1, product.id)}>
+                                        <i className="fa-solid fa-plus fa-lg"></i>
+                                    </button>
                                 </div>
-                                <div className="menu_text_page">
-                                    <div className="d-flex container-tittle-icon">  
-                                        <div className="menu_title_page">
-                                            <h4 className="menu_title">{product && product.nombre}</h4>
-                                        </div>
-                                        <button className="btn menu_icono"  onClick={(e) => handleBuyProduct(e, product.nombre, product.precio, product.descripcion, product.img, 1, product.id)}>
-                                            <i className="fa-solid fa-plus fa-lg"></i>
-                                        </button>
-                                    </div>
-                                    <div className="menu_price_data">
-                                            <h6 className="menu_price_page">{product && product.precio}$</h6>
-                                    </div>
-                                    <div className="menu_description_page">
-                                        <p>{product && product.descripcion}</p>
-                                    </div>
+                                <div className="menu_price_data">
+                                        <h6 className="menu_price_page">{product && product.precio}$</h6>
                                 </div>
-                        </div>
-                    )})}
+                                <div className="menu_description_page">
+                                    <p>{product && product.descripcion}</p>
+                                </div>
+                            </div>
                     </div>
-                  </div>
+                )})}
                 </div>
-              </div>
-            );
-          })}
         </div>
-      </div>
     </div>
-  );
-};
+)
+}
+
+
+
+
+
