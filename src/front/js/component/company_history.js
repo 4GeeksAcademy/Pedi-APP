cbdcbe146aefc7090e85f8e889e799bf47bb6d57
diuca puig <diucapuig@gmail.com>
2023-07-20 16:54:28 +0000

    fixed login final

--- src/front/js/component/company_history.js
+++ src/front/js/component/company_history.js
@@ -4,56 +4,56 @@ import { Context } from "../store/appContext";
 import "../../styles/userHistory.css";
 import Company_order from "./company_order";
 
+const Company_history = () => {
+  const { store, actions } = useContext(Context);
+  const [bills, setBills] = useState([]);
 
-const Company_history = () =>{
-    const {store,actions} = useContext(Context)
-    const [bills, setBills] = useState([])
+  useEffect(() => {
+    (async () => {
+      const token = localStorage.getItem("jwt-token");
+      try {
+        const response = await fetch(process.env.BACKEND_URL + "/api/bill", {
+          method: "POST",
+          body: JSON.stringify({
+            id: store.current_user_data.id,
+            role: store.current_user_data.role,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        });
+        const result = await response.json();
+        if (response.status == 401) {
+          Swal.fire(result.msg);
 
-    useEffect( () =>{
-        
-        (async () => {
-            
-            const token = localStorage.getItem('jwt-token');
-            try {
-                const response = await fetch(process.env.BACKEND_URL + "/api/bill", { 
-                    method : "POST",
-                    body: JSON.stringify({id : store.current_user_data.id, role :store.current_user_data.role}),
-                    headers: { 
-                        "Content-Type": "application/json",
-                        'Authorization': 'Bearer '+token
-                        } 
-                    
-                    
-                })
-                const result = await response.json()
-                if(response.status == 401){
-                    Swal.fire(result.msg)
-                    
-                    navigate("/", { replace: true });
+          navigate("/", { replace: true });
+        }
+        setBills(result.bills);
+      } catch (error) {
+        console.log("Error loading message from backend");
+      }
+    })();
+  }, [store.current_user_data]);
 
-                }
-                setBills(result.bills)
-                
-                
+  return (
+    <>
+      <h1 className="title_history_user">Your orders</h1>
+      {bills &&
+        bills.map((x, index) => {
+          return (
+            <Company_order
+              date={x.bill.fecha}
+              bill_id={x.bill.id}
+              user={x.user}
+              company_img={x.company_img}
+              time={x.bill.hora}
+              key={index}
+            />
+          );
+        })}
+    </>
+  );
+};
 
-            }catch(error){
-                console.log("Error loading message from backend")
-            }		
-        })()
-       
-
-    }, []);
-    
-    return(
-        <>
-            <h1 className="title_history_user">Your orders</h1>
-            {bills && (
-                        bills.map((x,index) =>{    
-                            return <Company_order date = {x.bill.fecha} bill_id = {x.bill.id} user={x.user} company_img={x.company_img} time= {x.bill.hora} key = {index}/>
-                        })
-                         )}
-        </>
-    )
-}
-
-export default Company_history
+export default Company_history;
