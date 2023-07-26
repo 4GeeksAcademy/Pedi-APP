4b1143bd33615dd81b8ca9c2dac16bec2fe3143f
diuca puig <diucapuig@gmail.com>
2023-07-26 12:10:12 +0000

    change footer and image in history

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,69 +1,97 @@
 import React, { Component } from "react";
 import "../../styles/footer.css";
-import logo from '../../img/DC.png';
+import logo from "../../img/DC.png";
 
 export const Footer = () => (
-  <footer className="py-3 border-top  footer_container">
+  <footer className="py-3 border-top  footer_container" id="footer_contain">
     <div className="row footer_row ">
-		<div className="col-6 col-lg-2 footer_col mt-3">
-			<div className="footer_logo mb-2">
-				<img type="button" className="btn btn-danger footer_logo_btn" src={logo} alt="Logo de la empresa" style={{ width: '55px'}}/>
-			</div>
-			<p className="footer_col1_text"> Follow us:</p>
-			<div className="footer_social">
-				<i className="fab fa-instagram fa-lg"></i>
-				<i className="fab fa-twitter fa-lg mx-3"></i>
-				<i className="fab fa-facebook-square fa-lg"></i>
-			</div>
+      <div className="col-6 col-lg-2 footer_col mt-3">
+        <div className="footer_logo mb-2">
+          <img
+            type="button"
+            className="btn btn-danger footer_logo_btn"
+            src={logo}
+            alt="Logo de la empresa"
+            style={{ width: "55px" }}
+          />
+        </div>
+        <p className="footer_col1_text"> Follow us:</p>
+        <div className="footer_social">
+          <i className="fab fa-instagram fa-lg"></i>
+          <i className="fab fa-twitter fa-lg mx-3"></i>
+          <i className="fab fa-facebook-square fa-lg"></i>
+        </div>
+      </div>
+      <div className="col-6 col-lg-2 footer_col mt-3">
+        <div className="footer_store mb-2 ">
+          <button
+            type="button"
+            className="btn btn-danger footer_store_btn mb-4 "
+          >
+            <div className="footer_appstore ">
+              <i className="fab fa-apple me-2"></i>
+              <p className="footer_appstore_text mt-3">
+                Download on the
+                <br />
+                <strong className="footer_appstore_text1 mt-3">
+                  App Store
+                </strong>
+              </p>
+            </div>
+          </button>
+          <button
+            type="button"
+            className="btn btn-danger footer_store_btn mb-4"
+          >
+            <div className="footer_appstore">
+              <i className="fab fa-google-play me-2"></i>
+              <p className="footer_google-play_text mt-3">
+                Get it on <br />
+                <strong className="footer_google-play_text1 mt-3">
+                  Google Play
+                </strong>
+              </p>
+            </div>
+          </button>
+        </div>
+      </div>
+      <div className="col-4 col-lg-2 footer_col  ">
+        <div className="footer_infotext_container ">
+          <p className="footer_infotext">Information</p>
+          <p className="footer_infosubtext">
+            {" "}
+            About us <br /> Contact <br />
+            Promotions
+            <br /> DishDash Prime
+          </p>
+        </div>
+      </div>
+      <div className="col-4 col-lg-2 footer_col  ">
+        <div className="footer_infotext_container ">
+          <p className="footer_infotext ">Lets do it together</p>
+          <p className="footer_infosubtext">
+            {" "}
+            Become a rider <br />
+            Add your business
+          </p>
 
-		</div>
-		<div className="col-6 col-lg-2 footer_col mt-3">
-			<div className="footer_store mb-2 ">
-				<button type="button" className="btn btn-danger footer_store_btn mb-4 ">
-					<div className="footer_appstore ">
-						<i className="fab fa-apple me-2"></i>
-						<p className="footer_appstore_text mt-3">Download on the<br/><strong className="footer_appstore_text1 mt-3">App Store</strong></p>
-					</div>
-					
-				</button>
-				<button type="button" className="btn btn-danger footer_store_btn mb-4">
-					<div className="footer_appstore">
-						<i className="fab fa-google-play me-2"></i>
-						<p className="footer_google-play_text mt-3">Get it on <br/><strong className="footer_google-play_text1 mt-3">Google Play</strong></p>
-					</div>
-					
-				</button>
-			</div>
-
-		</div>
-		<div className="col-4 col-lg-2 footer_col  ">
-			<div className="footer_infotext_container ">
-				<p className="footer_infotext">Information</p>
-				<p className="footer_infosubtext"> About us <br/> Contact <br/>Promotions<br/> DishDash Prime</p>
-			</div>
-
-		</div>
-		<div className="col-4 col-lg-2 footer_col  ">
-			<div className="footer_infotext_container ">
-				<p className="footer_infotext ">Lets do it together</p>
-				<p className="footer_infosubtext"> Become a rider <br/>Add your business</p>
-				
-				<div className="footer_languaje mb-auto">
-					<i className="fa-solid fa-language me-3 mb-1"></i>
-					<p className="footer_languaje_text mt-2">English</p>
-				</div>
-				
-			</div>
-
-		</div>
-		<div className="col-4 col-lg-2 footer_col  ">
-			<div className="footer_infotext_container ">
-				<p className="footer_infotext">Helpfull links</p>
-				<p className="footer_infosubtext"> Terms and conditions <br/> Privacy policy <br/>Cookies policy<br/> Compliance</p>
-				
-			</div>
-
-		</div>
-	</div>
+          <div className="footer_languaje mb-auto">
+            <i className="fa-solid fa-language me-3 mb-1"></i>
+            <p className="footer_languaje_text mt-2">English</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-4 col-lg-2 footer_col  ">
+        <div className="footer_infotext_container ">
+          <p className="footer_infotext">Helpfull links</p>
+          <p className="footer_infosubtext">
+            {" "}
+            Terms and conditions <br /> Privacy policy <br />
+            Cookies policy
+            <br /> Compliance
+          </p>
+        </div>
+      </div>
+    </div>
   </footer>
 );
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -25,60 +25,65 @@ import { CompanyPage } from "./pages/companyPage";
 
 import OrderDetail from "./pages/orderDetail";
 
-
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
 import CheckoutForm from "./pages/checkoutForm";
 import CompanyOrderHistory from "./pages/companyOrderHistory";
 
-
-
 //create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+  if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "")
+    return <BackendURL />;
 
-    const promise = loadStripe("pk_test_51NShHZGaOqlS5geCwKPZaHTaW5yq3C9nFeNqD13fcdrVdcp810PI3GeYW43IcsVqXTSg7ip0saQtuVnDq0FjvSyl00tpotDJnL");
+  const promise = loadStripe(
+    "pk_test_51NShHZGaOqlS5geCwKPZaHTaW5yq3C9nFeNqD13fcdrVdcp810PI3GeYW43IcsVqXTSg7ip0saQtuVnDq0FjvSyl00tpotDJnL"
+  );
 
+  return (
+    <div className="cont">
+      <BrowserRouter basename={basename}>
+        <Navbar />
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<SingupCliente />} path="/signupCliente" />
+          <Route element={<SingupEmpresa />} path="/signupEmpresa" />
+          <Route element={<Search />} path="/searchEmpresa" />
+          <Route element={<Signup />} path="/signup" />
+          <Route element={<Login />} path="/login" />
+          <Route element={<CompanyProfile />} path="/companyProfile" />
+          {
+            <Route
+              element={<CompanyOrderHistory />}
+              path="/companyOrderHistory"
+            />
+          }
+          <Route element={<MenuCompany />} path="/menu" />
+          <Route element={<CompanyAddProduct />} path="/addProduct" />
+          <Route element={<UserInfo />} path="/userProfile/info" />
+          <Route element={<UserHistory />} path="/userProfile/history" />
+          <Route element={<UserFavorites />} path="/userProfile/favorites" />
+          <Route element={<CompanyPage />} path="/companyPage/:idEmpresa" />
+          <Route element={<OrderDetail />} path="/orderDetail" />
+          <Route
+            path="/checkout"
+            element={
+              <Elements stripe={promise}>
+                <CheckoutForm />
+              </Elements>
+            }
+          />
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<SingupCliente/>} path="/signupCliente" />
-                        <Route element={<SingupEmpresa />} path="/signupEmpresa" />
-                        <Route element={<Search />} path="/searchEmpresa" />
-                        <Route element={<Signup />} path="/signup" />
-                        <Route element={<Login/>} path="/login"/>
-                        <Route element={<CompanyProfile />} path="/companyProfile"/>
-                        {<Route element={<CompanyOrderHistory/>} path="/companyOrderHistory"/> }
-                        <Route element={<MenuCompany/>} path="/menu"/>
-                        <Route element={<CompanyAddProduct />} path="/addProduct"/>
-                        <Route element={<UserInfo/>} path="/userProfile/info"/>
-                        <Route element={<UserHistory/>} path="/userProfile/history"/>
-                        <Route element={<UserFavorites/>} path="/userProfile/favorites"/>
-                        <Route element={<CompanyPage/>} path="/companyPage/:idEmpresa"/>
-                        <Route element={<OrderDetail/>} path="/orderDetail"/>
-                        <Route path="/checkout" element={
-                            <Elements stripe={promise}>
-                                <CheckoutForm />
-                            </Elements>
-                        }
-                        />
-                        
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-            </BrowserRouter>
-        </div>
-    );
+          <Route element={<h1>Not found!</h1>} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
-
